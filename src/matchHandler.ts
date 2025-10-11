a7e1eb5f13521b5b51713e54fed507a14f7fbc5e
unknown <sairamkonda9550@gmail.com>
2025-10-11 18:02:20 +0530

    sss

--- src/matchHandler.ts
+++ src/matchHandler.ts
@@ -1401,7 +1401,7 @@ const matchLeave = function (ctx: any,logger: any,nk: any,dispatcher: any,tick: 
   });
   logger.info("matchLeave called, players now:", Object.keys(state.presences));
   // Broadcast updated player status to all remaining players   
-  dispatcher.broadcastMessage(0,nk.stringToBinary(`UpdateMainPlayersData:${JSON.stringify(state.gameData)}`, Object.values(state.presences)));
+  dispatcher.broadcastMessage(0,nk.stringToBinary(`UpdateMainPlayersData:${JSON.stringify(state.gameData.players)}`, Object.values(state.presences)));
   return { state };
 };
 const matchLoop = function (ctx: any,logger: any,nk: any,dispatcher: any,tick: number,state: any,messages: any[]) {

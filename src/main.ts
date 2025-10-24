93e69a92952402abf7012fff988cdc6d4b7e8e22
unknown <sairamkonda9550@gmail.com>
2025-10-24 20:52:17 +0530

    n

--- src/main.ts
+++ src/main.ts
@@ -419,8 +419,10 @@ const rpcCreateRoom = function (ctx: any, logger: any, nk: any, payload: string)
   const boardIndex = data.boardIndex ?? 0;
   const numberOfPlayers = data.numberOfPlayers ?? 2;
   const gameMode = data.gameMode ?? "classic";
+  const fee = data.fee ?? 0;
+
   try {
-    const matchId = nk.matchCreate("lobby", { boardIndex, numberOfPlayers, gameMode, isPrivate: true });
+    const matchId = nk.matchCreate("lobby", { boardIndex, numberOfPlayers, gameMode,fee, isPrivate: true });
     logger.info(`✅ Private match created: ${matchId}`);
     return JSON.stringify({ matchId });
   } catch (err: any) {

a06c041a841b95f51a1a2ebc2b0f5a4ef4cc288e
unknown <sairamkonda9550@gmail.com>
2025-11-19 14:36:36 +0530

    n

--- src/matchHandler.ts
+++ src/matchHandler.ts
@@ -1813,58 +1813,34 @@ const matchLoop = function (ctx: any,logger: any,nk: any,dispatcher: any,tick: n
                     if (gameData.isWaitingForStealData) {
 
                         let stealData = gameData.stealData;
-
                         if (stealData) {
-
                             let fromWhoIndex = stealData.fromWhoIndex ?? 0;
                             let maxLettersToPick = stealData.maxLettersToPick;
                             let whoStealingIndex = stealData.whoStealingIndex;
-
                             // Only if it's this bot's steal turn
                             if (whoStealingIndex === WhosTurn) {
-
-                                let missingLetters =
-                                    gameData.WordGameState?.getMissingLettersListOfPlayer(whoStealingIndex);
-
-                                let collection =
-                                    gameData.WordGameState?.PlayerLetterCollections[fromWhoIndex];
-
+                                let missingLetters =gameData.WordGameState?.getMissingLettersListOfPlayer(whoStealingIndex);
+                                let collection =gameData.WordGameState?.PlayerLetterCollections[fromWhoIndex];
                                 if (missingLetters && collection && collection.length > 0) {
-
                                     let stealLetters: number[] = [];
                                     let neededLetters: string[] = Object.values(missingLetters);
-
                                     for (let i = 0; i < collection.length; i++) {
-
                                         for (let j = 0; j < neededLetters.length; j++) {
-
                                             if (neededLetters[j] === collection[i]) {
                                                 stealLetters.push(i);
                                                 break;
                                             }
                                         }
-
                                         if (stealLetters.length === maxLettersToPick) {
                                             break;
                                         }
                                     }
-
-                                    let signal = new Signal(
-                                        "wordoSaveSteal",
-                                        whoStealingIndex,
-                                        JSON.stringify(stealLetters)
-                                    );
-
-                                    matchSignal(
-                                        "",
-                                        logger,
-                                        nk,
-                                        dispatcher,
-                                        tick,
-                                        state,
-                                        JSON.stringify(signal)
-                                    );
+                                    let signal = new Signal("wordoSaveSteal",WhosTurn,JSON.stringify(stealLetters));
+                                    matchSignal("",logger,nk,dispatcher,tick,state,JSON.stringify(signal) );
                                 }
+                            }
+                            else{
+                              applyCommend(["message",{message:"whoStealingIndex === WhosTurn"}],state,dispatcher,nk);
                             }
                         }
                     }

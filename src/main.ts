2bbf445b7052bfddf87622d4ab03cbab57834ef6
unknown <sairamkonda9550@gmail.com>
2025-12-29 17:19:08 +0530

    n

--- src/main.ts
+++ src/main.ts
@@ -759,21 +759,15 @@ const mystery = function (ctx: any, logger: any, nk: any, payload: string): stri
         switch(m.type){
             case 1:
                 attendanceData.m.Collected = true;
-                if(attendanceData.meaningUnlockCards===null){
-                    attendanceData.meaningUnlockCards=0;
-                }
-                attendanceData.meaningUnlockCards+=1;
+                var c:number = attendanceData.meaningUnlockCards||0;
+                c+=1;
+                attendanceData.meaningUnlockCards=c;
                 break;
             case 2:
                 attendanceData.m.Collected = true;
-                if(attendanceData.pronounciationUnlockCards===null){
-                    attendanceData.pronounciationUnlockCards=0;
-                }
-                attendanceData.pronounciationUnlockCards+=1;
-                break;
-            case 3:
-                attendanceData.m.Collected = true;
-                attendanceData.m.type =0;
+                var c:number = attendanceData.pronounciationUnlockCards||0;
+                c+=1;
+                attendanceData.pronounciationUnlockCards=c;
                 break;
         }
         // --- SAVE UPDATED COINS ---

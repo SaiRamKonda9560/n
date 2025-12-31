458741cb998037e84ad5d204e6dbc952f68f3c24
unknown <sairamkonda9550@gmail.com>
2025-12-31 17:54:58 +0530

    n

--- src/matchHandler.ts
+++ src/matchHandler.ts
@@ -444,7 +444,7 @@ for (const player of this.players) {
         }
 
         // Use it in your function call
-        let level = getRandomLevelData(1,60);
+        let level = getRandomLevelData(55,60);
         let wordLength = level.wordLength;
         this.GenerateWordGameState(logger, nk, wordLength, level.uncommon, level.common, true, true, true, true);
     } else {

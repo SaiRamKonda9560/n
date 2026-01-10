2856cf148fa5ce8843f69d2696b5d4d805d1dc35
unknown <sairamkonda9550@gmail.com>
2026-01-11 04:41:51 +0530

    n

--- src/main.ts
+++ src/main.ts
@@ -1455,12 +1455,8 @@ const matchLoop_Tournament = function (ctx: any, logger: any, nk: any, dispatche
     return { state };
 };
 const matchTerminate_Tournament = function (ctx: any, logger: any, nk: any, dispatcher: any, tick: number, state: any, graceSeconds: number) {
-    logger.info("⭐⭐matchTerminate called, tick:", tick, "graceSeconds:", graceSeconds);
-    let objectIds = [
-    { collection: 'tournament', key: ctx.matchId ,userId:"00000000-0000-0000-0000-000000000000"},
-    ];
     try {
-        nk.storageDelete(objectIds);
+        nk.storageDelete([{ collection: 'tournament', key: ctx.matchId ,userId:"00000000-0000-0000-0000-000000000000"}]);
     } catch (error) {
         // Handle error
     }

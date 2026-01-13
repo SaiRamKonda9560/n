eda13c4a6a3495c67d2953f8c4ba274a8d770b33
unknown <sairamkonda9550@gmail.com>
2026-01-13 20:05:24 +0530

    n

--- src/main.ts
+++ src/main.ts
@@ -1550,13 +1550,11 @@ const matchInit_Tournament = function (ctx: any, logger: any, nk: any, params: a
     return { state:{}, tickRate: 1};
 };
 const matchJoinAttempt_Tournament = function (ctx: any, logger: any, nk: any, dispatcher: any, tick: number, state: any, presence: any, metadata: any) {
-    return { state }; 
+    return { state, accept: true }; 
 };
 const matchJoin_Tournament = function (ctx: any, logger: any, nk: any, dispatcher: any, tick: number, state: any, presences: any[]) {
   // Store new presences
-  presences.forEach(p => {
-    state.presences[p.sessionId] = p;
-  });
+
   return { state };
 };
 const matchLeave_Tournament = function (ctx: any,logger: any,nk: any,dispatcher: any,tick: number,state: any,presences: any[]){

3f4c455786a55feb88a603241d3c8ae251d75ee3
unknown <sairamkonda9550@gmail.com>
2026-01-13 18:01:14 +0530

    n

--- src/main.ts
+++ src/main.ts
@@ -1527,7 +1527,7 @@ const matchLoop_Tournament = function (ctx: any, logger: any, nk: any, dispatche
     }
     if(tick>32){
         try{
-            ctx.matchTerminate();
+            ctx.matchTerminate(); 
         }
         catch(e){
             state.matchTerminateError = e;

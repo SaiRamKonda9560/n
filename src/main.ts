af5e5006c79c0fb803bad3fe40e3a63b27ce4096
unknown <sairamkonda9550@gmail.com>
2026-01-15 14:43:10 +0530

    n

--- src/main.ts
+++ src/main.ts
@@ -1606,7 +1606,7 @@ const matchLoop_Tournament = function (ctx: any,logger: any,nk: any,dispatcher: 
       // ✅ SEND NOTIFICATION TO FIRST PLAYER
       if (length > 0) {
         for(let p of presences){
-            nk.notificationSend(p.userId,"hello", 0);
+            nk.notificationSend(p.userId,"hello");
         }
       }
       // ✅ START TOURNAMENT WHEN ALL PLAYERS JOINED

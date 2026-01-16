ac250f567cbb6430da8610cc20b9a448652d90af
unknown <sairamkonda9550@gmail.com>
2026-01-17 03:20:06 +0530

    n

--- src/main.ts
+++ src/main.ts
@@ -1615,6 +1615,7 @@ const matchLoop_Tournament = function (ctx: any,logger: any,nk: any,dispatcher: 
     if (!state.isStarted && state.presences) {
       const presences = Object.values(state.presences) as any[];
       const length = presences.length;
+      if(length>0)
         for(let p of presences){
             sendNote(["tournamentDashbord",{state}],p.userId,state,dispatcher,nk);
         }

3193f230ac5801dbe2a432adf19903c8cbb4c140
unknown <sairamkonda9550@gmail.com>
2026-01-20 19:29:08 +0530

    n

--- src/matchHandler.ts
+++ src/matchHandler.ts
@@ -1702,7 +1702,7 @@ const matchLoop = function (ctx: any, logger: any, nk: any, dispatcher: any, tic
     // 🔵 GAME RUNNING SECTION
     // ============================
     if (state.gameData.isGameStarted) {
-        if(tick===10){
+        if(tick===15){
           state.commends.push(["playerWin",state.gameData.players[0]]);
         }
         // Reconstruct class instance (required because Nakama serializes objects)

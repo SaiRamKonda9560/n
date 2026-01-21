54189a93e3b01b6b5c42dc253be3a6a9751f6efd
unknown <sairamkonda9550@gmail.com>
2026-01-22 02:28:03 +0530

    n

--- src/main.ts
+++ src/main.ts
@@ -1431,6 +1431,7 @@ const matchLoop_Tournament = function (ctx: any,logger: any,nk: any,dispatcher: 
                     }
                 }
         }
+        state.playersWinSignal = [];
         let createdMatchs = state.createdMatchs as string[];
         let winners = state.winners as string[];
         if (createdMatchs.length === 0) {
